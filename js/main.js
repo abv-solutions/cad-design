03a37573bdec3a64f0b0130f52e1df0443805f2f
Andrei Suciu <andreisuciu2005@yahoo.com>
2020-11-07 17:59:25 +0200

    improvements

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const toggle = document.querySelector('.toggle')
 const menuNav = document.querySelector('.menu-nav')
 const navItems = document.querySelectorAll('.nav-item')
+const txtElement = document.querySelector('.txt-type')
 
+document.addEventListener('DOMContentLoaded', () => TypeWriter.start())
 toggle.addEventListener('click', toggleMenu)
 
 function toggleMenu() {
@@ -11,5 +13,47 @@ function toggleMenu() {
   } else {
     menuNav.classList.remove('show')
     navItems.forEach((item) => item.classList.remove('show'))
+  }
+}
+
+// Type writer effect
+class TypeWriter {
+  constructor(txtElement, words, wait) {
+    this.txtElement = txtElement
+    this.words = words
+    this.txt = ''
+    this.wordIndex = 0
+    this.wait = parseInt(wait, 10)
+    this.isDeleting = false
+    this.type()
+  }
+  type() {
+    const current = this.wordIndex % this.words.length
+    const fullText = this.words[current]
+    let typeSpeed = 150
+    if (this.isDeleting) {
+      this.txt = fullText.substring(0, this.txt.length - 1)
+      typeSpeed /= 2
+    } else {
+      this.txt = fullText.substring(0, this.txt.length + 1)
+    }
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`
+    this.txtElement.style.display = 'inline'
+    if (!this.isDeleting && this.txt === fullText) {
+      this.isDeleting = true
+      typeSpeed = this.wait
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false
+      this.wordIndex++
+      typeSpeed = 500
+      this.txtElement.style.display = 'none'
+    }
+    // Cyclic function call
+    setTimeout(() => this.type(), typeSpeed)
+  }
+  static start() {
+    const words = JSON.parse(txtElement.getAttribute('data-words'))
+    const wait = txtElement.getAttribute('data-wait')
+    new TypeWriter(txtElement, words, wait)
   }
 }
